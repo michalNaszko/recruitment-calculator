34812f7ae18c4b536dfdc5fcb861b4fff235fd42
Michal <michal.naszko@gmail.com>
2023-09-09 12:34:17 +0200

    Calculator functions return null if input isnt numeric

--- src/Utils/Calculator.php
+++ src/Utils/Calculator.php
@@ -6,21 +6,21 @@ class Calculator
 {
     public static function add ($numA, $numB)
     {
-        return $numA + $numB;
+        return is_numeric($numA) and is_numeric($numB) ? ($numA + $numB) : null;
     }
 
     public static function subtract ($numA, $numB)
     {
-        return $numA - $numB;
+        return is_numeric($numA) and is_numeric($numB) ? ($numA - $numB) : null;
     }
 
     public static function multiply ($numA, $numB)
     {
-        return $numA * $numB;
+        return is_numeric($numA) and is_numeric($numB) ? ($numA * $numB) : null;
     }
 
     public static function divide ($numA, $numB)
     {
-        return $numA / $numB;
+        return is_numeric($numA) and is_numeric($numB) ? ($numA / $numB) : null;
     }
 }
--- tests/Unit/Calculator/CalculatorTest.php
+++ tests/Unit/Calculator/CalculatorTest.php
@@ -3,12 +3,23 @@
 namespace App\Tests\Unit\Calculator;
 
 use App\Utils\Calculator;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase
 {
     public function testAdd() : void
     {
+        // Only integers
         self::assertEquals(3, Calculator::add(1, 2));
+        self::assertEquals(153, Calculator::add(100, 53));
+
+        // One argument int and other float
+        self::assertEquals(33.5, Calculator::add(23.5, 10));
+        self::assertEquals(35.21, Calculator::add(15, 20.21));
+
+        // Only floats
+        self::assertEquals(13, Calculator::add(10.5, 2.5));
+        self::assertEquals(112.631, Calculator::add(12.2, 100.431));
     }
 }
