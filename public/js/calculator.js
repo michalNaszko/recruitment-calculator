8e5fd6236e99a2c6c506a1a716b1d8b191175a1d
Michal <michal.naszko@gmail.com>
2023-09-10 11:28:22 +0200

    Befor changing frontend concept

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
     $(".form-control").change(function(){
 
-        var numeric = new RegExp('^\\d+(?:.\\d+)?$');
-        if (numeric.test($(this).text())) {
+        var numeric = new RegExp('^\\d+(?:\.\\d+)?$');
+        if (numeric.test($(this).val())) {
             alert("text area with id: " + $(this).attr('id') + "is numeric");
         }
     });
--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Utils\Calculator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,36 @@ class CalculatorController extends AbstractController
     public function index(): Response
     {
         return $this->render('calculator.html.twig');
+    }
+
+    /**
+     * @Route("/{numA}/{operation}/{numB}", name="calculation",
+     *     requirements={"numA"="^\d+(?:\.\d+)?$", "operation"="\+|-|\*|:", "numB"="^\d+(?:\.\d+)?$"})
+     */
+    public function calculator(string $numA, string $operation, string $numB): Response
+    {
+        echo $numA . "<br/>";
+        echo $operation . "<br/>";
+        echo $numB . "<br/>";
+        echo $this->executeOperation($numA, $operation, $numB) . "<br/>";
+        return $this->render('calculator.html.twig', [
+            "result" => $this->executeOperation($numA, $operation, $numB)
+        ]);
+    }
+
+    private function executeOperation($numA, $operation, $numB)
+    {
+        switch ($operation) {
+            case "+":
+                return Calculator::add($numA, $numB);
+            case "-":
+                return Calculator::subtract($numA, $numB);
+            case "*":
+                return Calculator::multiply($numA, $numB);
+            case ":":
+                return Calculator::divide($numA, $numB);
+            default:
+                return null;
+        }
     }
 }
